3d60121d228d6678bc7fb5229d0c30ca8a415d71
musabz360 <musab@z360.biz>
2025-10-29 12:18:52 +0530

    configured for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/STRIDE/', // Set base path for GitHub Pages deployment
   root: 'public',
   build: {
     outDir: '../dist',
